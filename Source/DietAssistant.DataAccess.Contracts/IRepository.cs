dc6d32e9e4daa3a901c9440a97c9000e968ac368
Lyubomir Filipov <lyubomir.filipov@mentormate.com>
2022-05-30 09:15:57 +0300

    Impl repository pattern.

---
+++ Source/DietAssistant.DataAccess.Contracts/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace DietAssistant.DataAccess.Contracts
+{
+    public interface IRepository<TEntity> 
+        where TEntity : class
+    {
+        Task<TEntity> GetByIdAsync(Int32 id);
+
+        Task SaveAsync(TEntity entity);
+    }
+}
---
+++ Source/DietAssistant.DataAccess/Repositories/Repository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DietAssistant.DataAccess.Repositories
+{
+    public class Repository<TEntity>
+        where TEntity : class
+    {
+        protected readonly DietAssistantDbContext _dbContext;
+
+        protected Repository(DietAssistantDbContext dbContext)
+            => _dbContext = dbContext;
+
+        protected async Task<TEntity> GetByIdAsync(Int32 id)
+        {
+            var entity = await _dbContext
+                .Set<TEntity>()
+                .FindAsync(id)
+                .AsTask();
+
+            return entity;
+        }
+
+        protected async Task Delete(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task SaveEntityAsync(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Update(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
